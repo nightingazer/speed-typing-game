a024b8164ca66c1f5f556d8942955f6db0bebf41
nightingazer <nightingazer@outlook.com>
2021-06-06 17:16:07 +0300

    add timer, counting words, gamestate logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,46 @@
+import React, { useEffect, useState } from "react"
 import "../styles/App.scss"
 
 function App() {
-	return <div className="App">Hello</div>
+	const timer = 5
+	const [text, setText] = useState("")
+	const [timeRemaining, setTimeRemaining] = useState(timer)
+	const [isStarted, setIsStarted] = useState(false)
+
+	useEffect(() => {
+		setTimeout(() => {
+			if (timeRemaining > 0 && isStarted) {
+				setTimeRemaining((time) => time - 1)
+			} else {
+				setIsStarted(false)
+			}
+		}, 1000)
+	}, [timeRemaining, isStarted])
+
+	function handleChange(e) {
+		const { value } = e.target
+		setText(value)
+	}
+
+	function startGame() {
+		setIsStarted(true)
+		setTimeRemaining(timer)
+	}
+
+	function countWords() {
+		const wordCount = text.split(" ").filter((word) => word.trim() !== "").length
+		console.log(wordCount)
+	}
+
+	return (
+		<div className="App">
+			<h1>Speed Typing Game</h1>
+			<textarea value={text} onChange={handleChange} />
+			<h4>Time remaining: {timeRemaining}</h4>
+			<button onClick={startGame}>Start</button>
+			<h1> X Words</h1>
+		</div>
+	)
 }
 
 export default App
